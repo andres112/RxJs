a4b490f71517631660b7346eecb0fb631eeb8328
Andres Dorado <andres.dorado90@gmail.com>
2022-09-07 21:10:40 +0200

    first operator

--- src/operators/07-take.ts
+++ src/operators/07-take.ts
@@ -3,4 +3,6 @@ import { take } from 'rxjs/operators';
 
 const numbers$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-numbers$.pipe(take(5)).subscribe(console.log);
+numbers$
+  .pipe(take(5))
+  .subscribe({ next: console.log, complete: () => console.log('completed') });
