8605b33ffa2aac92f7d814bbbe199a328c692abf
Andres Dorado <andres.dorado90@gmail.com>
2022-09-12 17:24:01 +0200

    auditTime operator

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { interval, fromEvent, sample } from 'rxjs';
-
-const interval$= interval(500);
-const click$ = fromEvent(document, 'click');
-
-interval$.pipe(
-  sample(click$)
-).subscribe(console.log)
