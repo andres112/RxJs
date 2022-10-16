a8e83b132ddd5494b64d9abc10e38b021b8a1beb
Andres Dorado <andres.dorado90@gmail.com>
2022-10-16 18:46:43 +0200

    ajax, switchMap, zip exercise

--- src/rxjs-ejercicios-master/06-luke-skywalker.ts
+++ src/rxjs-ejercicios-master/06-luke-skywalker.ts
@@ -3,32 +3,25 @@ import { switchMap, map } from 'rxjs/operators';
 import { zip, of } from 'rxjs';
 
 /**
- * Ejercicio: 
+ * Ejercicio:
  *  Realizar 2 peticiones HTTP (ajax) una después de otra.
- *  
+ *
  *  La primera debe de obtener el personaje de Star Wars:
  *   Luke Skywalker, llamando el endpoint:   /people/1/
- * 
+ *
  *  La segunda petición, debe de ser utilizando el objeto
  *  de la petición anterior, y tomar la especie (species),
- *  que es un arreglo de URLs (array), dentro de ese arreglo, 
+ *  que es un arreglo de URLs (array), dentro de ese arreglo,
  *  tomar la primera posición y realizar la llamada a ese URL,
  *  el cual debería de traer información sobre su especie (Human)
  */
 
-// Respuesta esperada:
-// Información sobre los humanos en el universo de Star Wars
-// Ejemplo de la data esperada
-/*
- { name: "Human", classification: "mammal", designation: "sentient", average_height: "180", skin_colors: "caucasian, black, asian, hispanic", …}
-*/
-
-// Respuesta esperada con Mayor dificultad
+// Respuesta esperada
 // Retornar el siguiente objeto con la información de ambas peticiones
-// Recordando que se disparan una después de la otra, 
+// Recordando que se disparan una después de la otra,
 // con el URL que viene dentro del arreglo de 'species'
 
-// Tip: investigar sobre la función zip: 
+// Tip: investigar sobre la función zip:
 //      Que permite combinar observables en un arreglo de valores
 // https://rxjs-dev.firebaseapp.com/api/index/function/zip
 
@@ -38,28 +31,19 @@ import { zip, of } from 'rxjs';
     personaje: {name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", …}
 */
 
+(() => {
+  // No tocar ========================================================
+  const SW_API = 'https://swapi.dev/api';
+  const getRequest = (url: string) => ajax.getJSON<any>(url);
+  // ==================================================================
 
-(() =>{
+  // Realizar el llamado al URL para obtener a Luke Skywalker
 
-    // No tocar ========================================================
-    const SW_API = 'https://swapi.dev/api';                     
-    const getRequest = ( url: string ) => ajax.getJSON<any>(url);
-    // ==================================================================
-
-    // Realizar el llamado al URL para obtener a Luke Skywalker
-    getRequest(`Aquí va un URL`).pipe(
-        // Realizar los operadores respectivos aquí
-        
-
-
-        
-
-    // NO TOCAR el subscribe ni modificarlo ==
-    ).subscribe( console.log )           // ==
-    // =======================================
-
-
-
+  getRequest(`${SW_API}/people/1/`)
+    .pipe(
+      switchMap((resp) => zip(of(resp), getRequest(resp.vehicles[0]))),
+      map(([person, vehicle]) => ({ person, vehicle }))
+    )
+    .subscribe(console.log); // ==
+  // =======================================
 })();
-
-		
